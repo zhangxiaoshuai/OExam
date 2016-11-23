723868eba736d4fdfb18e8b43a87294a531647be
zhangxiaoshuai <zhang_xiao_shuai@yeah.net>
2016-11-23 11:06:36 +0800

    client token

---
+++ OExam.Client/Service/AuthToken.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OExam.Client.Service
+{
+    /// <summary>
+    /// TOKEN保存类
+    /// </summary>
+    class AuthToken
+    {
+        public static string TOKENNAME = "token";
+        private static string _tokenValue = "";
+        /// <summary>
+        /// 获取已经保存的token
+        /// </summary>
+        /// <returns></returns>
+        public static string GetToken()
+        {
+            return _tokenValue;
+        }
+        /// <summary>
+        /// 保存token
+        /// </summary>
+        /// <param name="token"></param>
+        public static void SaveToken(string token)
+        {
+            _tokenValue = token;
+        }
+    }
+}
